3fb47a70e0c106e885e06e8346fdcfa9ac501d0f
hrrs01 <haavard.hn@gmail.com>
2017-12-18 23:33:17 +0100

    latest update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
 
+#[macro_use]
+pub mod g_macro {
+    #[macro_export]
+    macro_rules! each {
+        ($vec_name:ident, $var_name:ident, $code:block) => (
+            for $var_name in $vec_name $code
+        )
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -6,9 +15,12 @@ mod tests {
     fn it_works() {
         use strings::StringExt;
         let mut test = String::from("Hello world");
-        test.pop_f();
-        assert_eq!('e', test.pop_f());
-        assert_eq!("llo world", &test);
+        //test.pop_f();
+        //assert_eq!('e', test.pop_f());
+        //assert_eq!("llo world", &test);
+        assert_eq!("H e l l o   w o r l d ",test.after_each(' '));
+        let test = vec!([1,2,3,4,5]);
+        each!(test, x, {println!("{:?}",x);})
     }
 }
 
@@ -16,8 +28,12 @@ pub mod strings {
 
     pub trait StringExt {
 
-        fn pop_f(&mut self) -> char;
 
+        //simple reverse pop function
+        fn pop_f(&mut self) -> char;
+        //function adding char after each char in string
+        // Filter support to be added?
+        fn after_each(&mut self, c: char) -> Self;
 
 
     }
@@ -25,6 +41,19 @@ pub mod strings {
         fn pop_f(&mut self) -> char {
             self.remove(0)
         }
+
+        fn after_each(&mut self, c: char ) -> Self {
+            let mut t: String = String::new();
+
+            for e in self.chars() {
+                t.push(e);
+                t.push(c);
+            }
+            t.clone()
+
+
+        }
+
     }
 
 }
