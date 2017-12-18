2635d998ba699bbbffb81ae6cb663c983d09769c
hrrs01 <haavard.hn@gmail.com>
2017-12-18 22:08:11 +0100

    Added first string addidtion

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
+
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        use strings::StringExt;
+        let mut test = String::from("Hello world");
+        test.pop_f();
+        assert_eq!('e', test.pop_f());
+        assert_eq!("llo world", &test);
     }
+}
+
+pub mod strings {
+
+    pub trait StringExt {
+
+        fn pop_f(&mut self) -> char;
+
+
+
+    }
+    impl StringExt for String {
+        fn pop_f(&mut self) -> char {
+            self.remove(0)
+        }
+    }
+
 }
